1bd2a4ec6930f8c4d47810c26536447d2a131445
Ashley <thy.ashley@gmail.com>
2020-06-06 18:02:23 +0800

    conditioning with JSX

--- 01_Basics/First_Component/index.js
+++ 01_Basics/First_Component/index.js
@@ -1,9 +1,17 @@
-class Hello extends React.Component {
+function getNum() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+class NumPicker extends React.Component {
     render() {
+        const num = getNum();
         return (
-            <h1>Hello There!</h1>
+            <div>
+                <h1>Your Number is { num }</h1>
+                <p>{ num === 7 ? 'Lucky!' : 'Unlucky' }</p>
+            </div>
         )
     }
 }
 
-ReactDOM.render(<Hello/>, document.querySelector('#root'))
+ReactDOM.render(<NumPicker/>, document.querySelector('#root'));
