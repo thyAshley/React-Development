659961d4b1b96b07e2a0f07482571a54b46c904a
Ashley <thy.ashley@gmail.com>
2020-06-07 19:17:10 +0800

    added router and switch

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
-import HelloWorld from "./Components/HelloWorld";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Home from "./views/Home";
+import About from "./views/About";
 
 function App() {
   return (
     <>
-      <Header />
-      <HelloWorld name="Josh" />
-      <Footer />
+      <Router>
+        <Header />
+
+        <div className="p-3">
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+
+        <Footer />
+      </Router>
     </>
   );
 }
