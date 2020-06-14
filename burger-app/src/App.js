2e509880c26ee7b8e5b20b3d4327900998696c66
Ashley <thy.ashley@gmail.com>
2020-06-14 17:21:33 +0800

    added layout components

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
 
-function App() {
-  return <div></div>;
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    );
+  }
 }
 
 export default App;
