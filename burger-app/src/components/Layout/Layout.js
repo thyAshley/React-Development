5adba7b4f5249ee1c828cfa0a2e8e0fb7c637471
Ashley <thy.ashley@gmail.com>
2020-06-14 17:31:44 +0800

    added burger component and layout.module.css

--- burger-app/src/components/Layout/Layout.js
+++ burger-app/src/components/Layout/Layout.js
@@ -1,10 +1,14 @@
 import React from "react";
+
 import Aux from "../../hoc/Auxiliary";
+import classes from "./Layout.module.css";
+
+console.log(classes);
 const Layout = (props) => {
   return (
     <Aux>
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>{props.children}</main>
+      <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
 };
