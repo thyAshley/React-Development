be08461d9f5f0b97d4b0945c04b154f9ca200000
Ashley <thy.ashley@gmail.com>
2020-06-17 19:22:01 +0800

    added withRouter as a higher order function

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 // import axios from 'axios';
 import axios from "../../axios";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
@@ -13,15 +14,23 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a href="/">New Post</a>
+                <Link
+                  to={{
+                    pathname: "/new-post",
+                    search: "?quick-submit=true",
+                  }}
+                >
+                  New Post
+                </Link>
               </li>
             </ul>
           </nav>
         </header>
-        <Route path="/" exact render={() => <Posts />} />
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
