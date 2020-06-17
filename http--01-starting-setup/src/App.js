a22a12392bd57579dbf1a4c3e661d19bcce28667
Ashley <thy.ashley@gmail.com>
2020-06-17 16:37:46 +0800

    refactoring

--- http--01-starting-setup/src/App.js
+++ http--01-starting-setup/src/App.js
@@ -1,39 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
 import Blog from "./containers/Blog/Blog";
 axios.defaults.baseURL = "https://my-json-server.typicode.com/typicode/demo";
-axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
-axios.defaults.headers.post["Content-Type"] = "application/json";
-
-axios.interceptors.request.use(
-  (request) => {
-    console.log(request);
-    return request;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
-
-axios.interceptors.response.use(
-  (response) => {
-    console.log(response);
-    return response;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Blog />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Blog />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,64 +1,27 @@
 import React, { Component } from "react";
-// import axios from "axios";
+// import axios from 'axios';
 import axios from "../../axios";
 
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
+import Posts from "./Posts/Posts";
 import "./Blog.css";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPost: null,
-    error: false,
-  };
-
-  componentDidMount() {
-    axios
-      .get("/posts")
-      .then((response) => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map((post) => {
-          return {
-            ...post,
-            author: "Max",
-          };
-        });
-        this.setState({ posts: updatedPosts });
-      })
-      .catch((err) => {
-        this.setState({ error: true });
-      });
-  }
-
-  postSelected = (id) => {
-    this.setState({ selectedPost: id });
-  };
-
   render() {
-    let posts = <p style={{ textAlign: "center" }}>Somethign went wrong!</p>;
-    if (!this.state.error) {
-      posts = this.state.posts.map((post) => {
-        return (
-          <Post
-            key={post.id}
-            clicked={() => this.postSelected(post.id)}
-            title={post.title}
-            author={post.author}
-          />
-        );
-      });
-    }
     return (
-      <div>
-        <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPost} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">New Post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Posts />
       </div>
     );
   }
