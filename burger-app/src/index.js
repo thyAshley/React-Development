acd164dcc509ccda27d1dd2230b2885fec5c8f36
Ashley <thy.ashley@gmail.com>
2020-06-28 12:11:34 +0800

    added redux-thunk and restructure folder

--- burger-app/src/index.js
+++ burger-app/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import reducer from "./store/reducer";
+import reducer from "./store/reducers/burgerBuilder";
 
 const logger = (store) => {
   return (next) => {
@@ -18,7 +18,8 @@ const logger = (store) => {
     };
   };
 };
-const store = createStore(reducer, applyMiddleware(logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(
   <Provider store={store}>
