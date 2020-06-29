a65a692bab56f1ea9e070d9e9c85ab5c6ef3cd23
Ashley <thy.ashley@gmail.com>
2020-06-29 20:05:10 +0800

    added auth page

--- burger-app/src/components/Navigation/NavItems/NavItems.js
+++ burger-app/src/components/Navigation/NavItems/NavItems.js
@@ -8,6 +8,7 @@ const NavItems = (props) => {
     <ul className={classes.NavItems}>
       <NavItem link="/">Burger Builder</NavItem>
       <NavItem link="/orders">Orders</NavItem>
+      <NavItem link="/auth">Authenticate</NavItem>
     </ul>
   );
 };
